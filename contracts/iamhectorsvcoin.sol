d281590f32b7124889622c389fa8365b39a5936c
Hector Steven Villasano <hectorstevenvillasano@gmail.com>
2022-03-04 14:20:17 -0800

    iamhectorsv is ownable

--- contracts/iamhectorsvcoin.sol
+++ contracts/iamhectorsvcoin.sol
@@ -2,13 +2,15 @@
 
 pragma solidity ^0.8.2; 
 
+import "./ownable.sol";
+
 /**
- * @title iamhectorsvcoin
- * @dev hectorsvill
+ * @title BEP20Token - iamhectorsv Token
+ * @dev A BEP20 Token for the Binances Smart Chain
  */
 
-contract BEP20Token {
-    mapping (address => uint256) private _balances;    
+contract iamhectorsv is Ownable {
+    mapping (address => uint256) internal _balances;    
     address private _owner;
     string private _name;
     string private _symbol;
@@ -19,45 +21,44 @@ contract BEP20Token {
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
     constructor() {
-        _name = "iamhectorsvcoin";
-        _symbol = "hsvc";
+        _name = "iamhectorsv";
+        _symbol = "hsv";
         _decimals = 16;
-        _totalSuply = 1000000 ** 10 * 16;
+        _totalSuply = 1000000 ** 10 * uint(_decimals);
         _balances[msg.sender] = _totalSuply;
         _owner =  msg.sender;
     }
 
-    function owner() public view returns (address) {
+    function owner() external view returns (address) {
         return _owner;
     } 
 
-    function name() public view returns (string memory) {
+    function name() external view returns (string memory) {
         return _name;
     }
 
-    function symbol() public view returns (string memory) {
+    function symbol() external view returns (string memory) {
         return _symbol;
     }
 
-    function decimals() public view returns (uint8) {
+    function decimals() external view returns (uint8) {
         return _decimals;
     }
 
-    function totalSuply() public view returns (uint256) {
+    function totalSuply() external view returns (uint256) {
         return _totalSuply;
     }
 
-    function balanceOf(address account) public view returns (uint256) {
+    function balanceOf(address account) external view returns (uint256) {
         return _balances[account];
     }
 
-    function transferFrom(address from, address to, uint value) public returns (bool) {
-        require(balanceOf(from) >= value, 'balance too low');
+    function transferFrom(address from, address to, uint value) external returns (bool) {
+        require(_balances[from] >= value, 'balance too low');
         _balances[to] += value;
         _balances[from] -= value;
         emit Transfer(from, to, value);
         return true;
     }
 
-    
 }
